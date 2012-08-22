bbd69cffa2185b65ca5de6569b8799f65d5be529
Tony Butler <tonyjbutler@gmail.com>
2012-08-22 23:41:01 +0100

    Replace favicon.ico with PNG

--- layout/header.php
+++ layout/header.php
@@ -91,7 +91,7 @@ echo $OUTPUT->doctype(); ?>
     <?php if ($usecf) { ?><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><?php } ?>
     <title><?php echo $PAGE->title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="<?php echo $OUTPUT->pix_url('favicon/favicon', 'theme'); ?>" />
+    <link rel="shortcut icon" type="image/png" href="<?php echo $OUTPUT->pix_url('favicon/favicon', 'theme'); ?>" />
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo $OUTPUT->pix_url('favicon/h/apple-touch-icon-precomposed', 'theme'); ?>">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo $OUTPUT->pix_url('favicon/m/apple-touch-icon-precomposed', 'theme'); ?>">
     <link rel="apple-touch-icon-precomposed" href="<?php echo $OUTPUT->pix_url('favicon/l/apple-touch-icon-precomposed', 'theme'); ?>">
